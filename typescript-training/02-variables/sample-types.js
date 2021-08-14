2be2d43ad683188157e5ecb1d84a08ecea1e09ed
ravindugm <ravindumiyuranga@gmail.com>
2021-08-14 15:14:40 +0530

    Add typescript/variables

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,15 @@
+var found = true;
+var grade = 98.2;
+var firstName = "Ravindu";
+var lastName = 'Miyuranga';
+// let's brake it
+/*
+found = 0;
+grade = "A";
+firstName = false;
+*/
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// use template string
+console.log("Hi " + firstName + " " + lastName);
---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,19 @@
+let found: boolean = true;
+let grade: number = 98.2;
+let firstName: string = "Ravindu";
+let lastName: string = 'Miyuranga';
+
+// let's brake it
+
+/*
+found = 0;
+grade = "A";
+firstName = false;
+*/
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+// use template string
+console.log(`Hi ${firstName} ${lastName}`);
