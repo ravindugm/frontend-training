b182100915151ba6d0a3c0ef08742a15a19eb5d2
ravindugm <ravindumiyuranga@gmail.com>
2021-09-05 00:01:56 +0530

    Angular/Creating a New Angular Component

---
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("John", "Doe", "john.doe@gmail.com", 50000),
+    new SalesPerson("Heinrich", "Treadway", "heinrich.treadway@gmail.com", 100000),
+    new SalesPerson("Evan", "McCauley", "evan.mcCauley@gmail.com", 20000),
+    new SalesPerson("Jason", "Bourn", "jason.bourn@gmail.com", 75000)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
